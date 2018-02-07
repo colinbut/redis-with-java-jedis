b7a9826843684a7a43cdfaa45478783bd33a8801
Colin But <colinbut@users.noreply.github.com>
2018-02-07 17:38:08 +0000

    move into separate classes

---
+++ src/main/java/com/mycompany/redis/jedis/datastructures/RedisCacheExample.java
@@ -0,0 +1,33 @@
+/*
+ * |-------------------------------------------------
+ * | Copyright © 2018 Colin But. All rights reserved.
+ * |-------------------------------------------------
+ */
+package com.mycompany.redis.jedis.datastructures;
+
+import redis.clients.jedis.Jedis;
+
+public class RedisCacheExample {
+
+    public static void main(String[] args) {
+
+        Jedis jedis = new Jedis();
+
+        // Simple Strings - K/V pair
+
+        /*
+         * Using it as a 'cache'
+         */
+
+        //storing it
+        jedis.set("events/city/paris", "32,15,233,328");
+
+        //getting it back
+        String value = jedis.get("events/city/paris");
+
+        // should be 32,15,233,328
+        System.out.println(value);
+
+        jedis.close();
+    }
+}
--- src/main/java/com/mycompany/redis/jedis/datastructures/RedisLists.java
+++ src/main/java/com/mycompany/redis/jedis/datastructures/RedisLists.java
@@ -3,29 +3,14 @@
  * | Copyright © 2018 Colin But. All rights reserved.
  * |-------------------------------------------------
  */
-package com.mycompany.redis.jedis;
+package com.mycompany.redis.jedis.datastructures;
 
 import redis.clients.jedis.Jedis;
 
-public class JedisProgram {
+public class RedisLists {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Jedis jedis = new Jedis();
-
-        // Simple Strings - K/V pair
-
-        /*
-         * Using it as a 'cache'
-         */
-
-        //storing it
-        jedis.set("events/city/paris", "32,15,233,328");
-
-        //getting it back
-        String value = jedis.get("events/city/paris");
-
-        // should be 32,15,233,328
-        System.out.println(value);
 
         // List
         jedis.lpush("queue#tasks", "first task");
@@ -34,6 +19,8 @@ public class JedisProgram {
 
         // pop the first task entered
         String firstTask = jedis.rpop("queue#tasks");
+
+        System.out.println(firstTask);
 
         jedis.close();
     }
