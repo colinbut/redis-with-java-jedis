526d2a946a457247688656edae9b770b577deca3
Colin But <colinbut@users.noreply.github.com>
2018-02-07 17:26:32 +0000

    jedis data structures - strings as key value cache

--- src/main/java/com/mycompany/redis/jedis/JedisProgram.java
+++ src/main/java/com/mycompany/redis/jedis/JedisProgram.java
@@ -11,6 +11,22 @@ public class JedisProgram {
 
     public static void main(String[] args){
         Jedis jedis = new Jedis();
+
+        // Simple Strings - K/V pair
+
+        /*
+         * Using it as a 'cache'
+         */
+
+        //storing it
+        jedis.set("events/city/paris", "32,15,233,328");
+
+        //getting it back
+        String value = jedis.get("events/city/paris");
+
+        // should be 32,15,233,328
+        System.out.println(value);
+
         jedis.close();
     }
 }
