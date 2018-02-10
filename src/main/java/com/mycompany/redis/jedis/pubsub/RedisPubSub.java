fa3c2a32c74fa3f0f5214e48746566889fb37a06
Colin But <colinbut@users.noreply.github.com>
2018-02-10 10:40:33 +0000

    some simple refactorings

--- src/main/java/com/mycompany/redis/jedis/pubsub/RedisPubSub.java
+++ src/main/java/com/mycompany/redis/jedis/pubsub/RedisPubSub.java
@@ -10,26 +10,31 @@ import redis.clients.jedis.JedisPubSub;
 
 public class RedisPubSub {
 
+    public static final String CHANNEL = "channel";
+
+    private static class SubscriberHandler {
+        JedisPubSub getJedisPubSub() {
+            return new JedisPubSub() {
+                @Override
+                public void onMessage(String channel, String message) {
+                    super.onMessage(channel, message);
+                    System.out.println("Recieved message: " + message + " on channel: " + channel);
+                }
+            };
+        }
+    }
+
     public static void main(String[] args) throws InterruptedException {
 
-        final String channel = "channel";
-        final boolean finishedSubscribing = false;
         final int terminateCondition = 1;
         final Jedis jedisSubscriber = new Jedis();
-
-        final JedisPubSub jedisPubSub = new JedisPubSub() {
-            @Override
-            public void onMessage(String channel, String message) {
-                super.onMessage(channel, message);
-                System.out.println("Recieved message: " + message + " on channel: " + channel);
-            }
-        };
+        final JedisPubSub jedisPubSub = new SubscriberHandler().getJedisPubSub();
 
         new Thread(new Runnable() {
             public void run() {
                 while (terminateCondition != 999) {
                     // this is a blocking method apparently hmmm...
-                    jedisSubscriber.subscribe(jedisPubSub, channel);
+                    jedisSubscriber.subscribe(jedisPubSub, CHANNEL);
                 }
             }
         }).start();
@@ -45,8 +50,6 @@ public class RedisPubSub {
                 jedisSubscriber.close();
             }
         }));
-
-
 
     }
 }
