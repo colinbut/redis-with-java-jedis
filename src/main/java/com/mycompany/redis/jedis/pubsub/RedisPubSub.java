45aad9ffddd87330c6a97fab69d93aeed6fcf77f
Colin But <colinbut@users.noreply.github.com>
2018-02-10 11:01:51 +0000

    Added publisher feature and refactored program example

--- src/main/java/com/mycompany/redis/jedis/pubsub/RedisPubSub.java
+++ src/main/java/com/mycompany/redis/jedis/pubsub/RedisPubSub.java
@@ -10,7 +10,23 @@ import redis.clients.jedis.JedisPubSub;
 
 public class RedisPubSub {
 
-    public static final String CHANNEL = "channel";
+    static final String CHANNEL = "channel";
+
+    static final int PROGRESS_TIME_IN_MS = 50000;
+
+    static final JedisPubSub jedisPubSub = new SubscriberHandler().getJedisPubSub();
+    static PublisherProducer publisherProducer = new PublisherProducer();
+    static SubscriberConsumer subscriberConsumer = new SubscriberConsumer();
+
+    private static boolean terminated = false;
+
+    private static void setTerminated() {
+        terminated = true;
+    }
+
+    private static boolean isTerminated(){
+        return terminated;
+    }
 
     private static class SubscriberHandler {
         JedisPubSub getJedisPubSub() {
@@ -24,27 +40,80 @@ public class RedisPubSub {
         }
     }
 
-    public static void main(String[] args) throws InterruptedException {
-
-        final int terminateCondition = 1;
+    private static class SubscriberConsumer {
         final Jedis jedisSubscriber = new Jedis();
-        final JedisPubSub jedisPubSub = new SubscriberHandler().getJedisPubSub();
 
-        new Thread(() -> {
-            while (terminateCondition != 999) {
-                // this is a blocking method apparently hmmm...
-                jedisSubscriber.subscribe(jedisPubSub, CHANNEL);
-            }
-        }).start();
+        void consumeMessage(JedisPubSub jedisPubSub) {
+            jedisSubscriber.subscribe(jedisPubSub, CHANNEL);
+        }
 
-        Thread.sleep(10000);
+        void closeSubscriberJedis() {
+            jedisSubscriber.close();
+        }
+    }
 
+    private static class PublisherProducer {
+        Jedis jedisPublisher = new Jedis();
+
+        void produceMessage() {
+            String message = "message";
+            System.out.println("Publishing message: " + message);
+            jedisPublisher.publish(CHANNEL, message);
+        }
+
+        void closePublisherJedis() {
+            jedisPublisher.close();
+        }
+    }
+
+    private static void cleanUpResourcesOnShutdown() {
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
             if (jedisPubSub.isSubscribed()) {
                 jedisPubSub.unsubscribe();
             }
-            jedisSubscriber.close();
+            subscriberConsumer.closeSubscriberJedis();
+            publisherProducer.closePublisherJedis();
         }));
+    }
 
+    public static void main(String[] args) throws InterruptedException {
+
+        Thread subscriberThread = new Thread(() -> {
+            try {
+                while (!isTerminated()) {
+                    // this is a blocking method apparently hmmm...
+                    subscriberConsumer.consumeMessage(jedisPubSub);
+
+                    if (Thread.interrupted()){
+                        throw new InterruptedException();
+                    }
+                }
+            } catch (InterruptedException ex) {
+                setTerminated();
+            }
+        });
+        subscriberThread.start();
+
+        Thread producerThread = new Thread(() -> {
+            try {
+                while (!isTerminated()) {
+                    publisherProducer.produceMessage();
+
+                    if (Thread.interrupted()){
+                        throw new InterruptedException();
+                    }
+                }
+            } catch (InterruptedException ex) {
+                setTerminated();
+            }
+        });
+        producerThread.start();
+
+        Thread.sleep(PROGRESS_TIME_IN_MS);
+
+        producerThread.interrupt();
+        subscriberThread.interrupt();
+
+        cleanUpResourcesOnShutdown();
     }
 }
